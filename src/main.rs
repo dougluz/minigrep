1bff8489de377c95bb2498137458c6340416f786
Douglas Luz <douglas.luz@zrp.com.br>
2021-02-07 20:46:24 -0300

    feat: added error handling to the run function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,18 +15,22 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
 
-fn run(config: Config) {
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut file = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
 
-    file.read_to_string(&mut contents)
-        .expect("somenthing went wrong reading the file");
+    file.read_to_string(&mut contents)?;
 
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
